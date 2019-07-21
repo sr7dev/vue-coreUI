6dfd5cfdf03e1f1b85987ea04ff373dc2864b13e
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-21 20:59:23 +0200

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -1,7 +1,12 @@
 export default {
   items: [
     {
-      
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'primary',
+        text: 'NEW'
       }
     },
     {
